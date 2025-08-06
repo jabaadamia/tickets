b382cb38e321a985e956f7f5597bf23f022e364d
unknown <jubapx123@gmail.com>
2025-08-06 22:55:47 +0400

    hello im juba

--- backend/src/main/java/ge/ticketebi/ticketebi_backend/controller/CategoryController.java
+++ backend/src/main/java/ge/ticketebi/ticketebi_backend/controller/CategoryController.java
@@ -2,10 +2,14 @@ package ge.ticketebi.ticketebi_backend.controller;
 
 import ge.ticketebi.ticketebi_backend.entities.Category;
 import ge.ticketebi.ticketebi_backend.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CategoryController {
@@ -19,4 +23,12 @@ public class CategoryController {
     public List<Category> getCategories() {
         return repository.findAll();
     }
+
+    @GetMapping("/categories/{name}")
+    public ResponseEntity<Category> getCategoryByName(@PathVariable String name) {
+        Optional<Category> category = repository.findById(name);
+        return category.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 }
