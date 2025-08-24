90b3028adabaf1ec7fc9be8162fcec9fb5777f03
jaba <jabaadamia1@gmail.com>
2025-08-24 22:35:57 +0400

    add MailService to send verification emails

---
+++ backend/src/main/java/ge/ticketebi/ticketebi_backend/security/MailService.java
@@ -0,0 +1,36 @@
+package ge.ticketebi.ticketebi_backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Value("${spring.mail.username}") private String email;
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendVerificationEmail(String toEmail, String link) {
+        String subject = "Verify your email";
+
+        String body = "Hello,\n\nPlease click the link below to verify your email:\n"
+                + link
+                + "\n\nThis link will expire in 24 hours.";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(email);
+
+        mailSender.send(message);
+    }
+}
