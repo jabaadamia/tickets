e0d1f0eebcfdd7fc458a005f9d9c66b69257a873
jaba <jabaadamia1@gmail.com>
2025-08-24 22:36:45 +0400

    add MessageResponse string responses

---
+++ backend/src/main/java/ge/ticketebi/ticketebi_backend/domain/dto/MessageResponse.java
@@ -0,0 +1,14 @@
+package ge.ticketebi.ticketebi_backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
